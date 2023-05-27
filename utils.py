e53f3d54c02ba52376b213abd047db2519d78254
James Yeom <43377225+yeomjh00@users.noreply.github.com>
2023-05-27 13:05:32 +0900

    print format

--- utils.py
+++ utils.py
@@ -79,7 +79,7 @@ def test(args, model: torch.nn.Module , test_set, cuda=True):
             accuracy_epoch.append(accuracy)
         
         with open(f"{args.output_path}/{str(args.aug_type)}.txt", "a+") as f:
-            f.write(f"{batch_idx}th batch: loss/acc: {loss_epoch}/{accuracy_epoch}")
+            f.write(f"{batch_idx}th batch: loss/acc: {loss.item()}/{accuracy}")
             f.write("\n")
 
         loss_mean_epoch = np.mean(loss_epoch)

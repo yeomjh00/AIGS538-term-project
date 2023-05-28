9a71a34d52fed249e146b6e55d2b0e8c0668871f
JamesYeom <duawogn@naver.com>
2023-05-28 21:42:53 +0900

    scripts

---
+++ sb_cutmix.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#SBATCH --job-name=cutmix
+#SBATCH -p titanxp
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH --gres=gpu:2
+
+# epoch is enough around 200~300
+
+srun python main.py --epochs=200 --batch_size=128 --aug_type=cutmix
